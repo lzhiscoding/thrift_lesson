bd3c453e7e0b737817ce65d882aeae0d2468d49b
lzh <liu__zihao@163.com>
2024-11-03 14:40:55 +0800

    match server version:2.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -9,6 +9,12 @@
 
 #include <iostream>
 
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <vector>
+
 using namespace std;
 
 using namespace ::apache::thrift;
@@ -16,7 +22,53 @@ using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
 
-using namespace  ::match_service;
+using namespace ::match_service;
+
+
+struct Task{
+	User user;
+	string type;
+};
+
+
+struct MessageQueue{
+	queue<Task> q;
+	mutex m;
+	condition_variable cv;
+
+}message_queue;
+
+class Pool{
+	public:
+		void save_result(int a,int b){
+			printf("Match Result: %d %d\n",a,b);
+		}
+
+		void match(){
+			while (users.size() > 1){
+				auto a = users[0],b = users[1];
+				users.erase(users.begin());
+				users.erase(users.begin());
+				save_result(a.id,b.id);
+			}
+		}
+
+		void add(User user){
+			users.push_back(user);
+		}
+
+		void remove(User user){
+			for(uint32_t i = 0;i < users.size();i++){
+				if(users[i].id == user.id){
+					users.erase(users.begin() + i);
+					break;
+				}
+			}
+		}
+	private:
+		vector<User> users;
+}pool;
+
 
 class MatchHandler : virtual public MatchIf {
 	public:
@@ -28,6 +80,11 @@ class MatchHandler : virtual public MatchIf {
 			// Your implementation goes here
 			printf("add_user\n");
 
+
+			unique_lock<mutex> lck(message_queue.m);
+			message_queue.q.push({user,"add"});
+			message_queue.cv.notify_all();
+
 			return 0;
 		}
 
@@ -35,10 +92,39 @@ class MatchHandler : virtual public MatchIf {
 			// Your implementation goes here
 			printf("remove_user\n");
 
+			unique_lock<mutex> lck(message_queue.m);
+			message_queue.q.push({user,"remove"});
+			message_queue.cv.notify_all();
+
 			return 0;
 		}
 
 };
+
+
+void consume_task(){
+	while(1){
+
+		unique_lock<mutex> lck(message_queue.m);
+
+		if(message_queue.q.empty()){
+			message_queue.cv.wait(lck);
+		}
+		else{
+			auto task = message_queue.q.front();
+			message_queue.q.pop();
+			lck.unlock();
+
+
+			if(task.type == "add") pool.add(task.user);
+			else if(task.type == "remove") pool.remove(task.user);
+
+			pool.match();
+
+		}
+	}
+}
+
 
 int main(int argc, char **argv) {
 	int port = 9090;
@@ -50,6 +136,9 @@ int main(int argc, char **argv) {
 
 	TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
 	cout << "start Match Server" << endl;
+
+	thread matching_thread(consume_task);
+
 	server.serve();
 	return 0;
 }
